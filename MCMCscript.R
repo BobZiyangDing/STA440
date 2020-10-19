7dfe703067a09cb690f4f31fa0293fd4297e4d89
BobZiyangDing <zd26@duke.edu>
2020-10-19 03:22:23 +0000

    finished rmd

--- MCMCscript.R
+++ MCMCscript.R
@@ -1,13 +1,88 @@
-GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
-{
+MCMC_noBlock <- function(train_win_X, train_win_Y, test_win_X, test_win_Y, p, total_itr=10000, kappa=0.02, remember=1000){
+  num_avg <- 195000
+  
+  set.seed(1234)
+  n_train <- length(train_win_X)
+  n_test <- length(test_win_X)
+  q <- dim(train_win_X[[1]])[2]
+
+  
+  ####################
+  ### Dynamic Init ###
+  ####################
+  
+  # Prior for balpha_0: Normal(m_0, C_0)
+  m_0 <- rep(0, p)
+  C_0 <- diag(p)
+  balpha_0 <- mvrnorm(n = 1, m_0, C_0)
+  
+  ##########################
+  ### Dynamic Model Init ###
+  ##########################
+  
+  # Prior for phi (w^{-1}) : Gamma(a_0, b_0)
+  a_0 <- 1/2
+  b_0 <- 1/2
+  phi <- rgamma(1, shape = a_0, rate = b_0)
+  w <- 1/phi
+  
+  # Prior for btheta_{1:p}: Normal(theta_mu_0, theta_Sigma_0)
+  theta_mu_0 <- rep(0, p)
+  theta_Sigma_0 <- diag(p) *0.01
+  btheta <- as.matrix(mvrnorm(n = 1, theta_mu_0, theta_Sigma_0/phi))
+  
+  ##############################
+  ### Observation Model Init ###
+  ##############################
+  
+  # Random initialize for tau (v): no density
+  tau <- 1
+  v <- 1/tau
+  
+  # Prior for beta_{1:q}: Normal(beta_mu_0, beta_Sigma_0)
+  beta_mu_0 <- rep(0, q)
+  beta_Sigma_0 <- diag(q) * ( kappa^-1) * (tau^-1)
+  bbeta <- as.matrix(mvrnorm(1, beta_mu_0, beta_Sigma_0))
+  
+  
+  ######################
+  ### Container Init ###
+  ######################
+  
   record_alphas <- NULL
   record_thetas <- NULL
   record_betas <- NULL
   record_ws <- NULL
   record_vs <- NULL
+  record_ms <- NULL
+  record_Cs <- NULL
+  record_res <- NULL
+  record_mse <- NULL
+  predictions <- as.list(rep(0, n_test))
   
-  for(i in 1:total_itr)
+  ####################
+  #### MCMC Start ####
+  # ####################
+  
+  
+  for(itr in 1:total_itr)
   {
+    if(itr %% 100 == 0){
+      message(cat("finished iteration ", itr))
+    }
+    m <- list()
+    C <- list()
+    a <- list()
+    R <- list()
+    ms <- list()
+    Cs <- list()
+    balphas <- list()
+    alphas <- c()
+    residuals <- list()  
+    a_pred <- list()
+    R_pred <- list()
+    mse <- c()
+    
     # Make \bTHETA
     jordan_mtx <- cbind(diag(p-1), rep(0, p-1))
     bTHETA <- rbind(t(btheta), jordan_mtx)
@@ -36,7 +111,7 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
       }
       else{
         a_i <- bTHETA %*% m[[i-1]]
-        R_i <- t(bTHETA) %*% C[[i-1]] %*% bTHETA+ w*diag(p)
+        R_i <- bTHETA %*% C[[i-1]] %*% t(bTHETA)+ w*diag(p)
         m_i <- solve(solve(R_i) + (1/v) * t(bm_1) %*% bm_1)  %*% 
           (solve(R_i)%*%a_i + 1/v * t(bm_1) %*%(train_win_Y[[i]] - train_win_X[[i]] %*% as.matrix(bbeta)) )
         C_i <- solve(solve(R_i) + (1/v) * t(bm_1) %*% bm_1)
@@ -47,7 +122,37 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
       }
     }
     
-    
+    ### Make a prediction
+    for(i in 1:n_test)
+    {
+      n_i <- dim(test_win_X[[i]])[1]
+      # Make \bm{1}
+      bm_1_row <- cbind(c(1), t(rep(0, p-1)))
+      bm_1 <- matrix(rep( t(bm_1_row), n_i), nrow=n_i, byrow=TRUE)
+      if(i==1)
+      {
+        a_pred_i <- bTHETA %*% m[[n_train]]
+        R_pred_i <- bTHETA %*% C[[n_train]] %*% t(bTHETA)+ w*diag(p)
+        a_pred[[i]] <- a_pred_i
+        R_pred[[i]] <- R_pred_i
+        y_pred <- bm_1 %*% a_pred_i + test_win_X[[i]] %*% as.matrix(bbeta)
+        if(itr>total_itr-num_avg)
+        {
+          predictions[[i]] <- predictions[[i]] + as.matrix(y_pred)
+        }
+      }
+      else{
+        a_pred_i <- bTHETA %*% a_pred[[i-1]]
+        R_pred_i <- bTHETA %*% R_pred[[i-1]] %*% t(bTHETA)+ w*diag(p)
+        a_pred[[i]] <- a_pred_i
+        R_pred[[i]] <- R_pred_i
+        y_pred <- bm_1 %*% a_pred_i + test_win_X[[i]] %*% as.matrix(bbeta)
+        if(itr>total_itr-num_avg)
+        {
+          predictions[[i]] <- predictions[[i]] + as.matrix(y_pred)
+        }
+      }
+    }
     
     #############################
     ##### Backward Sampling #####
@@ -56,20 +161,20 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
     # n_train = T  
     ms[[n_train]] = m[[n_train]]
     Cs[[n_train]] = C[[n_train]]
-    balpha_T <- mvrnorm(n = 1, m_0, C_0)
+    balpha_T <- mvrnorm(n = 1, ms[[n_train]], C[[n_train]])
     balphas[[n_train]] = balpha_T
     alphas[n_train] = balpha_T[1]
     for(i in (n_train-1):1)
     {
       # RTS smoother mean and variance
-      J_i <- C[[i]] %*% t(bTHETA) %*% solve(R[[i]])
+      J_i <- C[[i]] %*% t(bTHETA) %*% solve(R[[i+1]])
       ms_i <- m[[i]] + J_i %*% (ms[[i+1]] - bTHETA %*% m[[i]])
       Cs_i <- C[[i]] + J_i %*% (Cs[[i+1]] - R[[i+1]]) %*% t(J_i)
       
       # Backward sample Conditional balpha_t
       cond_ms_i <- m[[i]] + J_i %*% (balphas[[i+1]] - bTHETA %*% m[[i]])
       cond_Cs_i <- C[[i]] + J_i %*% R[[i+1]] %*% t(J_i)
-      balpha_i <-  mvrnorm(n = 1, cond_ms_i, cond_Cs_i)  
+      balpha_i <-  mvrnorm(n = 1, cond_ms_i, cond_Cs_i)
       
       # Record smoothed mean and variance
       ms[[i]] <- ms_i
@@ -82,7 +187,7 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
     
     # Deal with t=0 problem
     # RTS smoother mean and variance
-    J_0 <- C_0 %*% t(bTHETA) %*% solve(R[[i]])
+    J_0 <- C_0 %*% t(bTHETA) %*% solve(R[[1]])
     ms_0 <- m_0 + J_0 %*% (ms[[1]] - bTHETA %*% m_0)
     Cs_0 <- C_0 + J_0 %*% (Cs[[1]] - R[[1]]) %*% t(J_0)
     
@@ -90,10 +195,6 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
     cond_ms_0 <- m_0 + J_0 %*% (balphas[[1]] - bTHETA %*% m_0)
     cond_Cs_0 <- C_0 + J_0 %*% R[[1]] %*% t(J_0)
     balpha_0 <-  mvrnorm(n = 1, cond_ms_0, cond_Cs_0)  
-    
-    # # Update prior with posterior for future iterations
-    # m_0 <- ms_0
-    # C_0 <- Cs_0
     
     
     ##################################
@@ -104,13 +205,14 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
     Dyn_X <- t(cbind(balpha_0, t(do.call(rbind, balphas[c(1:n_train-1)]))) )
     
     
-    theta_mu_n <- solve( t(Dyn_X) %*% Dyn_X + theta_Sigma_0 ) %*% 
-      (theta_Sigma_0 %*% theta_mu_0 + t(Dyn_X) %*% Dyn_y)
-    theta_Sigma_n <- t(Dyn_X) %*% Dyn_X + theta_Sigma_0
+    theta_mu_n <- solve( t(Dyn_X) %*% Dyn_X + solve(theta_Sigma_0) ) %*% 
+      (solve(theta_Sigma_0) %*% theta_mu_0 + t(Dyn_X) %*% Dyn_y)
+    theta_Sigma_n <- solve(t(Dyn_X) %*% Dyn_X + solve(theta_Sigma_0))
+    
     a_n <- a_0 + n_train/2
     b_n <- b_0 + 1/2 * (  t(Dyn_y) %*% Dyn_y + 
-                            t(theta_mu_0) %*% theta_Sigma_0 %*% theta_mu_0 +
-                            t(theta_mu_n) %*% theta_Sigma_n %*% theta_mu_n )
+                            t(theta_mu_0) %*% solve(theta_Sigma_0) %*% theta_mu_0 -
+                            t(theta_mu_n) %*% solve(theta_Sigma_n) %*% theta_mu_n )
     
     # Sample and update w = phi^{-1}
     phi <- rgamma(1, shape = a_n, rate = b_n )
@@ -143,17 +245,45 @@ GodDamnItMCMC <- function(total_itr, p, q, n_train, train_win_X, train_win_Y)
     # Sample and update bbeta
     bbeta <- mvrnorm(1, beta_mu_n, beta_Sigma_n)
     
+    # Calculate residuals
+    res <- as.matrix(Obs_Z - Obs_X %*% bbeta)
+    
+    
     ##################################
     ##### record MCMC trajectory #####
     ##################################
     
-    record_alphas <- rbind(record_alphas, alphas)
-    record_thetas <- rbind(record_thetas, btheta)
-    record_betas <- rbind(record_betas, bbeta)
-    record_ws <- rbind(record_ws, w)
-    record_vs <- rbind(record_vs, v)
+    if(itr>total_itr-remember)
+    {
+      record_alphas <- rbind(record_alphas, alphas)
+      record_thetas <- rbind(record_thetas, btheta)
+      record_betas <- rbind(record_betas, bbeta)
+      record_ws <- rbind(record_ws, w)
+      record_vs <- rbind(record_vs, v)
+      record_res <- rbind(record_res, t(res))
+    }
   }
   
-  return( list(record_alphas,record_thetas,record_betas,record_ws,record_vs) )
+  #########################
+  ##### Calculate MSE #####
+  #########################
+  record_mse <- c()
+  average_y_pred <- list() 
+  for(j in n_test)
+  {
+     y_pred_j <- predictions[[j]] / num_avg
+     mse_j <- MSE(y_pred_j, test_win_Y[[j]])
+     record_mse[j] <- mse_j
+  }
+    
+    
+  results <- list("record_alphas" = record_alphas,
+                  "record_thetas" = record_thetas,
+                  "record_betas" = record_betas,
+                  "record_ws" = record_ws,
+                  "record_vs" = record_vs,
+                  "record_res" = record_res,
+                  "record_mse" = record_mse)
+  
+  return(results)
 }
-
